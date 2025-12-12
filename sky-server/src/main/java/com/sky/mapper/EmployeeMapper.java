f7d59c1ec904d8089d7e1db6b30ac4567879609a
41928 <419289657@qq.com>
2025-12-12 21:37:52 +0800

    公共字段自动填充代码实现

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,7 +1,9 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
@@ -14,7 +16,7 @@ public interface EmployeeMapper {
 
     /**
      * 根据用户名查询员工
-     * 
+     *
      * @param username
      * @return
      */
@@ -23,23 +25,25 @@ public interface EmployeeMapper {
 
     /**
      * 插入员工数据
-     * 
+     *
      * @param employee
      */
     @Insert("insert into employee (name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) "
             +
             "values (#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
+    @AutoFill(value = OperationType.INSERT)
     void insert(Employee employee);
 
     /**
      * 员工分页查询
-     * 
+     *
      * @param employeePageQueryDTO
      * @return
      */
     List<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
+    @AutoFill(value = OperationType.UPDATE)
     void update(Employee employee);
 
     Employee getById(Long id);
