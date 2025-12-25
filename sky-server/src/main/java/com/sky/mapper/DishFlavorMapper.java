5e427b29869eb4ccd6995562117e8d39bd07ec40
41928 <419289657@qq.com>
2025-12-25 17:39:52 +0800

    菜品查询

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -19,11 +19,12 @@ public interface DishFlavorMapper {
     /**
      * 根据菜品id查询对应的口味数据
      *
-     * @param dishId
+     * @param dishIds
      * @return
      */
-    @Delete("delete from dish_flavor where dish_id = #{dishId}")
-    void deleteByDishId(Long dishId);
+    void deleteByDishIds(List<Long> dishIds);
+//    @Delete("delete from dish_flavor where dish_id = #{dishId}")
+//    void deleteByDishId(Long dishId);
 
-    void deleteByDishIds(List<Long> ids);
+
 }
